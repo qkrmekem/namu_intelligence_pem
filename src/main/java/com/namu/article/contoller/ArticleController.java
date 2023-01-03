74bd93d1b11b5cbba9bb4c1ca6e17aafccbe6282
qkrmekem <magicofclown@naver.com>
2023-01-04 01:44:50 +0900

    주석 작성, 코드 수정

--- src/main/java/com/namu/article/contoller/ArticleController.java
+++ src/main/java/com/namu/article/contoller/ArticleController.java
@@ -24,6 +24,7 @@ public class ArticleController {
     @Autowired
     ArticleService articleService;
 
+    // 페이지 정보를 넘겨 받지 않았을 때 띄울 메인페이지
     @RequestMapping
     public String main(Model model){
         int total_article = articleService.getTotalArticle();
@@ -34,6 +35,7 @@ public class ArticleController {
         return "articleList";
     }
 
+    // 페이지 정보를 넘겨 받았을 때 띄울 메인페이지
     @RequestMapping("/{page}")
     public String main(Model model, @PathVariable int page){
         int total_article = articleService.getTotalArticle();
@@ -44,16 +46,19 @@ public class ArticleController {
         return "articleList";
     }
 
+    //게시판 글 작성 페이지를 띄우기 위한 메서드
     @RequestMapping("articleForm")
     public String articleForm(){
         return "articleForm";
     }
 
+    //게시판 글 작성 중 취소 버튼을 눌렀을 때 메인 페이지로 이동
     @RequestMapping("resetInsert")
     public String resetInsert(){
         return "redirect:/";
     }
 
+    //썸머노트에서 이미지 드롭다운이나 복사 붙여넣기 이벤트 발생 시에 작동하는 콜백 함수
     @ResponseBody
     @RequestMapping("/uploadSummernoteImageFile")
     public JsonObject uploadSummernoteImageFile(@RequestParam("file") MultipartFile multipartFile) {
@@ -63,12 +68,14 @@ public class ArticleController {
         return jsonObject;
     }
 
+    //글쓰기 작성 완료 버튼 누를 때 작동하는 메서드
     @RequestMapping("/insertArticle")
     public String insertArticle(Article article, MultipartFile multipartFile) throws Exception{
         articleService.insertArticle(article, multipartFile);
         return "redirect:/";
     }
 
+    //게시판 글 상세보기 페이지로 이동하는 메서드
     @RequestMapping("/article/{a_seq}")
     public String article(@PathVariable int a_seq, Model model){
         articleService.updateArticleCount(a_seq);
@@ -77,6 +84,7 @@ public class ArticleController {
         return "article";
     }
 
+    //게시글 수정 페이지로 이동하는 메서드
     @RequestMapping("/updateArticleForm/{a_seq}")
     public String updateArticleForm(@PathVariable int a_seq, Model model){
         Article article = articleService.getArticle(a_seq);
@@ -84,29 +92,23 @@ public class ArticleController {
         return "articleUpdateForm";
     }
 
+    // 게시글 수정 완료 시에 DB에 적용시키고 해당 게시글로 다시 이동
     @RequestMapping("/updateArticle")
     public String updateArticle(Article article, MultipartFile multipartFile) throws Exception{
         articleService.updateArticle(article, multipartFile);
         return "redirect:/article/"+article.getA_seq();
     }
 
+    // 게시글 삭제 메서드
     @RequestMapping("/deleteArticle/{a_seq}")
     public String deleteArticle(@PathVariable int a_seq){
         articleService.deleteArticle(a_seq);
         return "redirect:/";
     }
-//
-//    ResourceLoader resourceLoader;
-//
-//    @Autowired
-//    public void FileController (ResourceLoader resourceLoader) {
-//        this.resourceLoader = resourceLoader;
-//    }
 
-
+    //파일 다운로드 할 때 작동하는 메서드
     @GetMapping("/download/{filename}")
     public void download(HttpServletResponse response, @PathVariable String filename) throws IOException {
-        System.out.println("여기까지 옴");
         String path = "C:/Users/10/Desktop/article/src/main/resources/static/files/"+filename;
 
         byte[] fileByte = FileUtils.readFileToByteArray(new File(path));
@@ -120,6 +122,7 @@ public class ArticleController {
         response.getOutputStream().close();
     }
 
+    // 댓글 버튼 클릭 시에 댓글 목록을 가져오는 비동기 통신 메서드
     @RequestMapping("/getComments")
     @ResponseBody
     public  List<Comment> getComments(@RequestParam int a_seq, Model model){
@@ -127,6 +130,7 @@ public class ArticleController {
         return list;
     }
 
+    //댓글 삭제 시에 작동하는 메서드
     @RequestMapping("/deleteComments")
     @ResponseBody
     public List<Comment> deleteComments(@RequestParam int c_seq, @RequestParam int a_seq, Model model){
@@ -135,6 +139,7 @@ public class ArticleController {
         return list;
     }
 
+    //댓글 작성 시에 작동하는 메서드
     @RequestMapping("/insertComments")
     @ResponseBody
     public List<Comment> insertComments(Comment comment){
